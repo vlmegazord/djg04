1f9d8b039648aea9b41c4da9a2eb1ad6f70e71da
Vassily <vasil.deville@gmail.com>
2017-07-25 00:53:15 +0300

    Show the queryset on the basic html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone as tz
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(date_published__lte=tz.now()).order_by('-date_published')
+    return render(request, 'blog/post_list.html', {'posts': posts})
