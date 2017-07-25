bc6a31e5b006f2a5be105a6630a05483b31e9d93
Vassily <vasil.deville@gmail.com>
2017-07-25 13:18:52 +0300

    Add the plain submit form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone as tz
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,7 @@ def post_detail(request, pk):
     # post = Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
