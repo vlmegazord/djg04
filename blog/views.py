71044be06a63c298b4fff8ed63b16f170925a3c9
Vassily <vasil.deville@gmail.com>
2017-07-25 13:56:24 +0300

    Add the NewPost

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone as tz
+
+from .models import Post
 from .forms import PostForm
 
 # Create your views here.
@@ -14,5 +15,15 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author = request.user
+            post.date_published = tz.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
