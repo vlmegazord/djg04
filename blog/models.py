fc26c931e342cb619873782ae33d2b4304862afc
Vassily <vasil.deville@gmail.com>
2017-07-25 22:16:37 +0300

    Add users' comments

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    date_created = models.DateTimeField(default=tz.now)
+    is_approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.is_approved=True
+        self.save()
+    def __str__(self):
+        return self.text
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone as tz
 from django.contrib.auth.decorators import login_required
 
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -59,6 +59,19 @@ def post_remove(request, pk):
     post=get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post= post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form})
 # def post_edit(request, pk):
 #     post = get_object_or_404(Post, pk)
 #     if request.method == 'POST':
